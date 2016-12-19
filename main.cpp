3a39ebfe907eb57276d93c0b0a4187a314e012dc
ProjectSONAR <ProjectSONAR@email.com>
2016-12-19 00:14:53 -0500

    Small updates...

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow win(sf::VideoMode(200, 200), "SFML Test");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow win(sf::VideoMode(683, 384), "Sonar Battle");
+
+    sf::Clock mainClock;
+    sf::Time acc = sf::Time::Zero;
+    sf::Time timestep = sf::seconds(1.f / 30.f);
+
+    sf::Font defaultFont;
+
+    if (!defaultFont.loadFromFile("assets/kenpixel.ttf"))
+    {
+        std::cout << "Error loading font!" << std::endl;
+    }
+
+    sf::Text fpsCounter = sf::Text("FPS: ", defaultFont, 15);
+    fpsCounter.setOutlineColor(sf::Color::Black);
+    fpsCounter.setOutlineThickness(1.f);
+    fpsCounter.setPosition(500.f, 25.f);
 
     while (win.isOpen())
     {
+        // Events should not be limited by FPS
         sf::Event event;
         while (win.pollEvent(event))
         {
@@ -17,8 +33,16 @@ int main()
             }
         }
 
-        win.clear();
-        win.draw(shape);
+        while (acc > timestep)
+        {
+            acc -= timestep;
+
+            //TODO: Gamelogic here
+        }
+
+        // FPS is limited by the computer; we don't need to do anything
+        win.clear(sf::Color(95,145,220, 255));
+        win.draw(fpsCounter);
         win.display();
     }
 
