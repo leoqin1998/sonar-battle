d86f7f6f9cf611f95c9ea04a565bc0dc72b8e45f
ProjectSONAR <ProjectSONAR@email.com>
2016-12-19 00:29:48 -0500

    Added new colours!

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
 int main()
 {
-    sf::RenderWindow win(sf::VideoMode(200, 200), "SFML Test");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow win(sf::VideoMode(683, 384), "Sonar Battle");
+
+    sf::Clock mainClock;
+    sf::Time acc = sf::Time::Zero;
+    sf::Time timestep = sf::seconds(1.f/ 30.f);
+
+    sf::Font defaultFont;
+
+    if (defaultFont.loadFromFile("assets/kenpix.ttf"))
+    {
+        std::cout << "Error loading font!" << std::endl;
+    }
+
+    sf::Text fpsCounter = sf::Text("FPS: ", defaultFont, 25);
+    fpsCounter.setPosition(500.f, 25.f);
 
     while (win.isOpen())
     {
+        // Event handling should not be capped
         sf::Event event;
         while (win.pollEvent(event))
         {
@@ -17,8 +32,17 @@ int main()
             }
         }
 
-        win.clear();
-        win.draw(shape);
+        // Game logic should be capped by timestep
+        while (acc > timestep)
+        {
+            acc -= timestep;
+
+            //TODO: Do game logic here
+        }
+
+
+        // Drawing should not be capped
+        win.clear(sf::Color(120, 165, 240));
         win.display();
     }
 
